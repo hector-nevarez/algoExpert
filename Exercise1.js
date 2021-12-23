9f7a660e2879030664d5aef05aa61be954b6af5b
hector nevarez <hanevarezg@gmail.com>
2021-12-23 13:35:01 -0800

    Finished the efficient solution

--- Exercise1.js
+++ Exercise1.js
@@ -14,27 +14,60 @@
 
 // ------------------------Code below----------------------------
 
+// function twoNumberSum(array, targetSum) {
+//     // Write your code here.
+//     // ----------Pseudo code------------ 
+//     // Since the array is NOT sorted will use nested  for loops to go through the entire array
+//     // If while looping through the second for loop the sum of both elements match the targetSum the add both elements into an array --> else keep looping
+//     // return the populated array
+//     const result = [];
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i + 1; j < array.length; j++) {
+//             sum = array[i] + array[j];
+//             if (sum === targetSum) {
+//                 result.push(array[i]);
+//                 result.push(array[j]);
+//             }
+//         }
+//     }
+//     return result;
+// // ^^^^^^^^^^^^^^^^^^^YAY IT WORKS^^^^^^^^^^^^^^^^
+// // My time complexity is O(n^2) Space not sure but probably O(n)
+// }
+
 function twoNumberSum(array, targetSum) {
     // Write your code here.
     // ----------Pseudo code------------ 
-    // Since the array is NOT sorted will use nested  for loops to go through the entire array
-    // If while looping through the second for loop the sum of both elements match the targetSum the add both elements into an array --> else keep looping
-    // return the populated array
+    // Considering X+Y=targetSum --> Having targetSum & X known we attempt to find Y by doing Y= targetSum - X
+    // Know with that Y value we store that value inside an object. 
+    // Once we have all Y values we loop through the array and if we find the Y value we store X & Y inside the result array
     const result = [];
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i + 1; j < array.length; j++) {
-            sum = array[i] + array[j];
-            if (sum === targetSum) {
-                result.push(array[i]);
-                result.push(array[j]);
-            }
+    const yValuesRegistry = {};
+    let y = 0;
+    let x = 0;
+    for (let j = 0; j < array.length; j++) {
+        y = targetSum - array[j];
+        if (!yValuesRegistry[y] && y !== array[j]) {
+            yValuesRegistry[y] = y;
         }
     }
+    // console.log('This is the Y values stored:', yValuesRegistry);
+    for (let i = 0; i < array.length; i++) {
+        x = array[i];
+        // console.log('This is X', x);
+        // console.log('This is yValuesRegistry of x:', yValuesRegistry[x]);
+        if (yValuesRegistry[x] || yValuesRegistry[x] === 0) {
+            y = yValuesRegistry[x];
+            result.push(x);
+        }
+    }
+    console.log('This is the Result:', result);
     return result;
-// ^^^^^^^^^^^^^^^^^^^YAY IT WORKS^^^^^^^^^^^^^^^^
-// My time complexity is O(n^2) Space not sure but probably O(n)
+    // ^^^^^^^^^^^^^^^^^^^YAY IT WORKS^^^^^^^^^^^^^^^^
+    // My time complexity is O(n) Space not sure but probably O(n)
 }
 
+twoNumberSum([0, 5], 5);
 // Do not edit the line below.
 // exports.twoNumberSum = twoNumberSum;
