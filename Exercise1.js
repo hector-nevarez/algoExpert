8b17afc3a2978730731fb0829276e0cc3812b8f4
hector nevarez <hanevarezg@gmail.com>
2021-12-23 13:50:42 -0800

    Added an algoexpert solution

--- Exercise1.js
+++ Exercise1.js
@@ -14,27 +14,27 @@
 
 // ------------------------Code below----------------------------
 
-// function twoNumberSum(array, targetSum) {
-//     // Write your code here.
-//     // ----------Pseudo code------------ 
-//     // Since the array is NOT sorted will use nested  for loops to go through the entire array
-//     // If while looping through the second for loop the sum of both elements match the targetSum the add both elements into an array --> else keep looping
-//     // return the populated array
-//     const result = [];
-//     let sum = 0;
-//     for (let i = 0; i < array.length; i++) {
-//         for (let j = i + 1; j < array.length; j++) {
-//             sum = array[i] + array[j];
-//             if (sum === targetSum) {
-//                 result.push(array[i]);
-//                 result.push(array[j]);
-//             }
-//         }
-//     }
-//     return result;
-// // ^^^^^^^^^^^^^^^^^^^YAY IT WORKS^^^^^^^^^^^^^^^^
-// // My time complexity is O(n^2) Space not sure but probably O(n)
-// }
+function twoNumberSum(array, targetSum) {
+    // Write your code here.
+    // ----------Pseudo code------------ 
+    // Since the array is NOT sorted will use nested  for loops to go through the entire array
+    // If while looping through the second for loop the sum of both elements match the targetSum the add both elements into an array --> else keep looping
+    // return the populated array
+    const result = [];
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            sum = array[i] + array[j];
+            if (sum === targetSum) {
+                result.push(array[i]);
+                result.push(array[j]);
+            }
+        }
+    }
+    return result;
+// ^^^^^^^^^^^^^^^^^^^YAY IT WORKS^^^^^^^^^^^^^^^^
+// My time complexity is O(n^2) Space not sure but probably O(n)
+}
 
 function twoNumberSum(array, targetSum) {
     // Write your code here.
@@ -71,3 +71,22 @@ function twoNumberSum(array, targetSum) {
 twoNumberSum([0, 5], 5);
 // Do not edit the line below.
 // exports.twoNumberSum = twoNumberSum;
+
+// Extra solution directly from Algoexpert:
+function twoNumberSum(array, targetSum) {
+    array.sort((a, b) => a - b)
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        const currentSum = array[left] + array[right];
+        if (currentSum === targetSum) {
+            return [array[left], array[right]]
+        } else if (currentSum < targetSum) {
+            left++;
+        } else if (currentSum > targetSum) {
+            right++;
+        }
+    }
+    return [];
+}
+// ^^^^^^^^^^ Time Complexity: O(nlog(n)), Space: O(1) ^^^^^^^^^^^^^^^^
